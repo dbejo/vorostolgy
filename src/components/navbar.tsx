60ae0e1deedab5f865ac1ba51aced817b4e83be9
dbejo <david.bejo@genesy.com>
2023-11-22 08:56:26 +0100

    Use Link instead of a tag

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,20 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <div className="sticky top-0 flex h-12 w-full justify-center gap-6 bg-red-700 text-white">
-      <a href="/" className="self-center">
+      <Link href="/" className="self-center">
         Rólunk
-      </a>
-      <a href="/menu" className="self-center">
+      </Link>
+      <Link href="/menu" className="self-center">
         Étlap
-      </a>
-      <a href="/" className="self-center">
+      </Link>
+      <Link href="/" className="self-center">
         Galéria
-      </a>
-      <a href="/" className="self-center">
+      </Link>
+      <Link href="/" className="self-center">
         Elérhetőségek
-      </a>
+      </Link>
     </div>
   );
 }
