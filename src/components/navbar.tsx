3aaf9f244af64ec94c1f10a6f6d5033f37e3f22d
dbejo <david.bejo@genesy.com>
2023-11-24 12:28:40 +0100

    Improve navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,75 @@
 import Link from "next/link";
+import { useState } from "react";
+
+const HamburgerButton = (props: { isButtonClicked?: boolean }) => {
+  return (
+    <button className="group relative">
+      <div className="relative flex h-10 w-10 items-center justify-center overflow-hidden rounded bg-red-900 shadow-md">
+        <div
+          className={`flex h-[20px] w-[20px] flex-col justify-between overflow-hidden transition-all duration-200 ${
+            props.isButtonClicked ? "-translate-y-1.5 -rotate-90" : ""
+          }`}
+        >
+          <div
+            className={`h-[2px] w-7 bg-white transition-all duration-200 ${
+              props.isButtonClicked
+                ? "w-2/3 origin-left rotate-[42deg]"
+                : "rotate-0"
+            }`}
+          ></div>
+          <div
+            className={`h-[2px] w-7 bg-white transition-all duration-200 ${
+              props.isButtonClicked ? "hidden" : "block"
+            }`}
+          ></div>
+          <div
+            className={`h-[2px] w-7 bg-white transition-all duration-200 ${
+              props.isButtonClicked
+                ? "w-2/3 origin-left -rotate-[42deg]"
+                : "rotate-0"
+            }`}
+          ></div>
+        </div>
+      </div>
+    </button>
+  );
+};
 
 export default function Navbar() {
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
   return (
-    <div className="sticky top-0 flex h-12 w-full justify-center gap-6 bg-red-700 text-white">
-      <Link href="/" className="self-center">
-        Rólunk
-      </Link>
-      <Link href="/etlap" className="self-center">
-        Étlap
-      </Link>
-      <Link href="/galeria" className="self-center">
-        Galéria
-      </Link>
-      <Link href="/" className="self-center">
-        Elérhetőségek
-      </Link>
+    <div className="sticky top-0 w-full bg-red-700 text-white">
+      <div className="flex h-14 md:hidden">
+        <div className="grow self-center">
+          <Link href="/" className="flex w-fit px-2">
+            <h1 className="text-lg">Vörös Tölgy</h1>
+          </Link>
+        </div>
+        <div
+          className="px-2"
+          onClick={() => setIsButtonClicked(!isButtonClicked)}
+        >
+          <HamburgerButton isButtonClicked={isButtonClicked} />
+        </div>
+      </div>
+      <div
+        className={`flex flex-col justify-center transition-all duration-200 md:h-12 md:flex-row md:gap-6 ${
+          isButtonClicked ? "" : "hidden md:flex"
+        }`}
+      >
+        <Link href="/" className="self-center p-2 md:p-0">
+          Rólunk
+        </Link>
+        <Link href="/etlap" className="self-center p-2 md:p-0">
+          Étlap
+        </Link>
+        <Link href="/galeria" className="self-center p-2 md:p-0">
+          Galéria
+        </Link>
+        <Link href="/" className="self-center p-2 md:p-0">
+          Elérhetőségek
+        </Link>
+      </div>
     </div>
   );
 }
