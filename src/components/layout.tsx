e4a985213ac961795181946b510cded8b707d63e
dbejo <david.bejo@genesy.com>
2023-11-27 08:34:48 +0100

    Remove overscroll-y-scroll from layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from "react";
 export const PageLayout = (props: PropsWithChildren<unknown>) => {
   return (
     <main className="flex justify-center">
-      <div className="h-full w-full overflow-y-scroll p-4 md:max-w-6xl">
+      <div className="h-full w-full p-4 md:max-w-6xl">
         {props.children}
       </div>
     </main>
