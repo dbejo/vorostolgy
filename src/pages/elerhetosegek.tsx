baf48c777b284373345f7b998364bd84a4274a27
dbejo <david.bejo@genesy.com>
2023-11-28 17:49:24 +0100

    Add elerhetosegek page

---
+++ src/pages/elerhetosegek.tsx
@@ -0,0 +1,11 @@
+import { PageLayout } from "~/components/layout";
+import Navbar from "~/components/navbar";
+
+export default function ElerhetosegekPage() {
+  return (
+    <>
+      <Navbar />
+      <PageLayout>Hello</PageLayout>
+    </>
+  );
+}
